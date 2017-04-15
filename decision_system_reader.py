530837d3c53d867b7e59f9eca76b4f30a4496ee8
LS1991 <ls.sadowski@gmail.com>
2017-04-16 00:12:56 +0200

    Added: window interface, function to read rules data. Final version of program.

--- decision_system_reader.py
+++ decision_system_reader.py
@@ -1,5 +1,6 @@
 import tools
 from tkinter import *
+from tkinter import ttk
 import tkinter.filedialog as filedialog
 import tkinter.messagebox as messagebox
 
@@ -28,11 +29,40 @@ class MainFrame(Frame):
         self.system_text_box.pack(fill=X, padx=5, expand=True)
         self.system_text_box.configure(state=DISABLED)
 
-        start_button_frame = Frame(self)  # 2nd frame // GO!
-        start_button_frame.pack(fill=X)
+        rb_frame = Frame(self)  # 2nd frame // radio buttons and GO!
+        rb_frame.pack(fill=X)
 
-        self.start_button = Button(start_button_frame, text="GO!", state=DISABLED, command=self.__get_decision_system)
+        self.algorithm = IntVar()
+
+        self.radio_button1 = Radiobutton(rb_frame, text="Covering", variable=self.algorithm, value=1)
+        self.radio_button1.pack(side=LEFT, padx=5, pady=5)
+        self.radio_button1.invoke()
+
+        self.radio_button2 = Radiobutton(rb_frame, text="Exhaustive", variable=self.algorithm, value=2)
+        self.radio_button2.pack(side=LEFT, padx=5, pady=5)
+
+        self.radio_button3 = Radiobutton(rb_frame, text="LEM2", variable=self.algorithm, value=3)
+        self.radio_button3.pack(side=LEFT, padx=5, pady=5)
+
+        self.start_button = Button(rb_frame, text="GO!", state=DISABLED, command=self.__get_decision_system)
         self.start_button.pack(padx=5, pady=5, fill=X)
+
+        tree_frame = Frame(self)  # 3rd frame // tree
+        tree_frame.pack(fill=BOTH, expand=True)
+
+        scrollbar = Scrollbar(tree_frame)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.tree = ttk.Treeview(tree_frame, selectmode=NONE)
+        self.tree.pack(padx=5, pady=5, fill=BOTH, expand=True)
+
+        scrollbar.configure(command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar.set)
+
+        self.tree.heading('#0', text='Rules')
+        self.tree["columns"] = "count"
+        self.tree.column("count", width=1)
+        self.tree.heading("count", text="Count")
 
     def __get_system_filename(self):
         self.system_file_path = filedialog.askopenfilename(filetypes=[('Txt files', '*.txt')])
@@ -43,21 +73,39 @@ class MainFrame(Frame):
         if self.system_text_box.get() != '':
             self.start_button.config(state=NORMAL)
 
+    def insert_rules(self, rules):
+        self.tree.delete(*self.tree.get_children())
+        self.tree.insert("", 1, 1, text="All classes", values=rules.__len__())
+        scales = tools.get_scales(rules)
+        for scale in scales:
+            self.tree.insert(1, scale+1, scale+1, text="Scale {}".format(scale),
+                             values=tools.get_rule_scale_length(rules, scale))
+            for string_rule in tools.scale_rules(rules, scale):
+                self.tree.insert(scale+1, scale+1, text=string_rule)
+
     def __get_decision_system(self):
         try:
             with open(self.system_file_path) as file:
                 decision_system, names = tools.get_system_objects(file)
-            rules = tools.lem2(decision_system)
+            rules = tools.find_rules(self.__get_algorithm(), decision_system)
             tools.rename_rules(rules, names)
-            tools.print_rules(rules)
+            self.insert_rules(rules)
         except FileNotFoundError:
             messagebox.showerror("Error", "Oops! File not found!")
+
+    def __get_algorithm(self):
+        if self.algorithm.get() == 1:
+            return tools.covering
+        elif self.algorithm.get() == 2:
+            return tools.exhaustive
+        elif self.algorithm.get() == 3:
+            return tools.lem2
 
 
 def main():
     main_frame = Tk()
     ex = MainFrame(main_frame)
-    main_frame.geometry("500x350+500+300")
+    main_frame.geometry("660x450+420+200")
     main_frame.mainloop()
 
 
--- tools.py
+++ tools.py
@@ -1,12 +1,11 @@
 import itertools as it
 import classes
 import collections
-import numpy as np
-from typing import Dict
 
 
+# Functions for read rule based system and transform symbols to integers. <---------------------------------------------
 def get_system_objects(system_file):
-    """Return numpy array with uint16 that represent Rule Based System and dictionary to encode true values."""
+    """Return numpy array with integers that represent Rule Based System and dictionary to encode true values."""
     system_file.seek(0)  # return to beginning of file
     objects = []
     unique_values = {}
@@ -30,16 +29,22 @@ def get_object(line, unique_values):
 get_object.counter = 0
 
 
-# ----------------------------------------------------------------------------
-
-
-def covering(decision_system):
-    rules = []
-
+# Base function for all algorithms. <-----------------------------------------------------------------------------------
+def find_rules(algorithm, decision_system):
+    """Return rules calculeted by function from argument."""
+    rules = []  # all rules from current algorithm
     number_of_attributes = len(decision_system[0]) - 1  # number of attributes
     attributes = [attribute_index for attribute_index in range(number_of_attributes)]  # list of attributes ids
-    number_of_objects = decision_system.__len__()  # number of decision objects in decision system
 
+    algorithm(decision_system, rules, attributes)
+    return rules
+
+
+# Covering base function and support functions. <-----------------------------------------------------------------------
+def covering(decision_system, rules, attributes):
+    """Calculate rules by sequential covering."""
+    number_of_attributes = attributes.__len__() # number of attributes
+    number_of_objects = decision_system.__len__()  # number of decision objects in decision system
     eliminated = set()  # indexes of object that don't need to calculate
 
     for scale in range(number_of_attributes):
@@ -54,25 +59,8 @@ def covering(decision_system):
                         rules.append(rule)
                         break
                 if end(eliminated.__len__(), number_of_objects):
-                    return rules
-
-    return rules
-
-
-def is_rule_inconsistent(rule, objects):
-    for decision_object in objects:
-        if rule.decision != decision_object[-1] and has_object_fulfill_rule(rule, decision_object):
-            return False
-    else:
-        return True
-
-
-def has_object_fulfill_rule(rule, decision_object):
-    for key, value in rule.descriptors.items():
-        if value != decision_object[key]:
-            return False
-    else:
-        return True
+                    return
+    return
 
 
 def set_rule_support_and_eliminate(rule, objects, eliminated):
@@ -84,18 +72,13 @@ def set_rule_support_and_eliminate(rule, objects, eliminated):
 
 
 def end(number_of_eliminated, number_of_objects):
+    """Return: True if all objects was used; False if find not covered object."""
     return number_of_eliminated == number_of_objects
 
 
-# ----------------------------------------------------------------------------
-
-
-def exhaustive(decision_system):
-    rules = []
-
-    number_of_attributes = len(decision_system[0]) - 1  # number of attributes
-    attributes = [att for att in range(number_of_attributes)]  # list of attributes ids
-
+# Exhaustive base function and support functions. <---------------------------------------------------------------------
+def exhaustive(decision_system, rules, attributes):
+    """Calculate rules by exhaustive algorithm (with indistinguishable matrix)."""
     matrix = get_matrix(decision_system)
 
     for index, matrix_object in enumerate(matrix):
@@ -109,6 +92,7 @@ def exhaustive(decision_system):
 
 
 def get_matrix(decision_system):
+    """Return indistinguishable matrix."""
     matrix = []
     for current_object in decision_system:
         matrix.append(get_row(current_object, decision_system))
@@ -116,6 +100,7 @@ def get_matrix(decision_system):
 
 
 def get_row(current_object, decision_system):
+    """Return row of indistinguishable matrix."""
     row = []
     for decision_object in decision_system:
         row.append(get_cell(current_object, decision_object))
@@ -123,6 +108,7 @@ def get_row(current_object, decision_system):
 
 
 def get_cell(current_object, decision_object):
+    """Return cell of indistinguishable matrix."""
     cell = []
     if current_object[-1] == decision_object[-1]:
         return cell
@@ -140,6 +126,7 @@ def all_combinations(attributes):
 
 
 def is_combination_in_cell(cell, combination):
+    """Return: True if combination of attributes is in cell of indistinguishable matrix; False if not."""
     for attribute in combination:
         if attribute not in cell:
             return False
@@ -147,6 +134,7 @@ def is_combination_in_cell(cell, combination):
 
 
 def is_combination_in_row(row, combination):
+    """Return: True if combination is in object; False if not."""
     for cell in row:
         if is_combination_in_cell(cell, combination):
             return True
@@ -154,6 +142,7 @@ def is_combination_in_row(row, combination):
 
 
 def has_rule_contains_rule(first, second):
+    """Return: True if all descriptors from second rule is in first rule; False if at lest one descriptor is not in."""
     for key, value in second.descriptors.items():
         if key not in first.descriptors or first.descriptors[key] != value:
             return False
@@ -161,6 +150,7 @@ def has_rule_contains_rule(first, second):
 
 
 def is_rule_in_rules(rule, rules):
+    """Return: True if at least one rule from rules contains rule from argument; False if rule not contains."""
     for added_rule in rules:
         if has_rule_contains_rule(rule, added_rule):
             return True
@@ -168,59 +158,46 @@ def is_rule_in_rules(rule, rules):
 
 
 def set_rule_support(rule, objects):
-    """Calculate support of rule."""
+    """Calculate support of rule (in exhaustive algorithm)."""
     for decision_object in objects:
         if has_object_fulfill_rule(rule, decision_object) and rule.decision == decision_object[-1]:
             rule.support += 1
 
 
-# ----------------------------------------------------------------------------
-
-
-def lem2(decision_system):
-    number_of_attributes = len(decision_system[0]) - 1  # number of attributes
-    attributes = [att for att in range(number_of_attributes)]  # list of attributes ids
-    rules = []
-
+# LEM2 base function and support functions. <---------------------------------------------------------------------------
+def lem2(decision_system, rules, attributes):
+    """Calculate rules by LEM2 algorithm (Learn from Examples by Modules)."""
     unique_decisions = get_unique(decision[-1] for decision in decision_system)
     for decision in unique_decisions:
         concept_objects = get_concept_objects(decision_system, decision)
         while concept_objects:
             descriptors = {}
             tmp_attributes = attributes[:]
-
-            descriptor = get_descriptor(concept_objects, tmp_attributes)
-            add_descriptor(descriptors, descriptor)
-            remove_attribute(tmp_attributes, descriptor)
-            mode_objects = get_mode_objects(concept_objects, descriptors)
-            rule = classes.Rule(descriptors.keys(), mode_objects[0], descriptors.keys().__len__())
-            if not is_rule_inconsistent(rule, decision_system):
-                find_deeper(mode_objects, descriptors, concept_objects, tmp_attributes, decision_system, rules)
-            else:
-                set_rule_support_lem(rule, mode_objects, concept_objects)
-                rules.append(rule)
-    return rules
+            find_lem_rules(concept_objects, descriptors, concept_objects, tmp_attributes, decision_system, rules)
 
 
-def find_deeper(mode_objects, descriptors, concept_objects, tmp_attributes, decision_system, rules):
+def find_lem_rules(mode_objects, descriptors, concept_objects, tmp_attributes, decision_system, rules):
+    """Find all rules by recursion."""
     descriptor = get_descriptor(mode_objects, tmp_attributes)
     add_descriptor(descriptors, descriptor)
     remove_attribute(tmp_attributes, descriptor)
     mode_objects = get_mode_objects(mode_objects, descriptor)
     rule = classes.Rule(descriptors.keys(), mode_objects[0], descriptors.keys().__len__())
     if not is_rule_inconsistent(rule, decision_system):
-        find_deeper(mode_objects, descriptors, concept_objects, tmp_attributes, decision_system, rules)
+        find_lem_rules(mode_objects, descriptors, concept_objects, tmp_attributes, decision_system, rules)
     else:
         set_rule_support_lem(rule, mode_objects, concept_objects)
         rules.append(rule)
 
 
-def remove_attribute(attriubutes, descriptor):
+def remove_attribute(attributes, descriptor):
+    """Remove attribute from list of attributes."""
     for att in descriptor:
-        attriubutes.remove(att)
+        attributes.remove(att)
 
 
 def add_descriptor(descriptors, descriptor):
+    """Add descriptor to list of descriptors."""
     for key, value in descriptor.items():
         descriptors[key] = value
 
@@ -233,9 +210,10 @@ def set_rule_support_lem(rule, mode_objects, concept_objects):
             rule.support += 1
 
 
-def get_mode_objects(concept_objects, descriptors):
+def get_mode_objects(objects, descriptors):
+    """Return objects that contains descriptors."""
     mode_objects = []
-    for decision_object in concept_objects:
+    for decision_object in objects:
         for key, value in descriptors.items():
             if decision_object[key] == value:
                 mode_objects.append(decision_object)
@@ -243,6 +221,7 @@ def get_mode_objects(concept_objects, descriptors):
 
 
 def get_concept_objects(decision_system, decision):
+    """Return objects with decision from argument."""
     concept_objects = []
     for decision_object in decision_system:
         if decision_object[-1] == decision:
@@ -251,6 +230,7 @@ def get_concept_objects(decision_system, decision):
 
 
 def get_descriptor(concept_objects, attributes):
+    """Return mode descriptor from concept objects."""
     mode_descriptor = {}
     most_common = 0
 
@@ -271,6 +251,25 @@ def get_concept_column(concept_objects, attribute):
     return column
 
 
+# Universal tools. <----------------------------------------------------------------------------------------------------
+def is_rule_inconsistent(rule, objects):
+    """Return: True if rule is inconsistent; False if not."""
+    for decision_object in objects:
+        if rule.decision != decision_object[-1] and has_object_fulfill_rule(rule, decision_object):
+            return False
+    else:
+        return True
+
+
+def has_object_fulfill_rule(rule, decision_object):
+    """Return: True if object fulfill rule; False if not."""
+    for key, value in rule.descriptors.items():
+        if value != decision_object[key]:
+            return False
+    else:
+        return True
+
+
 def get_unique(array):
     """Return list of unique values"""
     unique = []
@@ -280,8 +279,8 @@ def get_unique(array):
     return unique
 
 
-# ----------------------------------------------------------------------------
 def rename_rules(rules, names):
+    """Transform rules from integers to original symbolic values."""
     for rule in rules:
         real_values = {}
         for key, value in rule.descriptors.items():
@@ -289,7 +288,28 @@ def rename_rules(rules, names):
         rule.descriptors = real_values
         rule.decision = names.get(rule.decision)
 
+
 def print_rules(rules):
+    """Print all rules on console."""
     for rule in rules:
         rule.print_rule()
-# ----------------------------------------------------------------------------
+
+
+def get_scales(rules):
+    scales = []
+    for rule in rules:
+        if rule.scale not in scales:
+            scales.append(rule.scale)
+    scales.sort()
+    return scales
+
+
+def scale_rules(rules, scale):
+    rules_scale = [rule for rule in rules if rule.scale == scale]
+    for scale_rule in rules_scale:
+        yield scale_rule.print_rule()
+
+
+def get_rule_scale_length(rules, scale):
+    rules_scale = [rule for rule in rules if rule.scale == scale]
+    return rules_scale.__len__()
